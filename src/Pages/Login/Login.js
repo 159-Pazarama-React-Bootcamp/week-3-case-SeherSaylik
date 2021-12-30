d40ebab442fd9719ea0537872195a76d431c17f9
SeherSaylik <sehersaylik@users.noreply.github.com>
2021-12-30 23:29:59 +0300

    login page created and avatar aligned

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <img src={require('../../images/avatar_vespa.png')} alt="login-page-avatar-vespa" className='image-avatar-vespa'/>
+    </div>
+  );
+}
+
+export default Login;
