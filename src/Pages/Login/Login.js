80175e143d0a1a969ee0acdc6eb8b42a2831425b
SeherSaylik <sehersaylik@users.noreply.github.com>
2022-01-01 17:13:14 +0300

    login page container and image is aligned

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,18 @@
 import './Login.css';
+import TransparentVespaAvatar from '../../images/transparent_avatar_vespa.png'
 import VespaAvatar from '../../images/avatar_vespa.png'
 
 function Login() {
   return (
     <div className="Login-Page">
-      <img src={VespaAvatar} alt="login-page-avatar-vespa" className='image-avatar-vespa'/>
+      <img src={TransparentVespaAvatar} alt="login-page-transparent-avatar-vespa" className='image-transparent-avatar-vespa'/>
       <div className='white-full-container'>
-        <div className='white-container-left-part'></div>
+        <div className='full-login-box'>
+            <a className='your-logo'>Your Logo</a>
+            <a className='login-text'>Login</a>
+        </div>
+        <div className='white-container-right-part'> </div>
+        <img src={VespaAvatar} alt="login--avatar-vespa" className='image-avatar-vespa'/>
       </div>
     </div>
   );
